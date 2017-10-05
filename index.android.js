24a236e908b258c9c060012ce8ba7095b3d08b00
Michal Krawiec <mki@axxiome.com>
2017-10-05 12:55:56 +0200

    newCommit

--- index.android.js
+++ index.android.js
@@ -17,7 +17,8 @@ export default class todolist extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native!Axxiome
+
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
