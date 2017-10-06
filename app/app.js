e6d14dcd31dee66077b8b3d915ecd069e45c600d
Kamil Obst <obst.kamil@gmail.com>
2017-10-06 12:09:23 +0200

    Add logo at startup with timeout

--- app/app.js
+++ app/app.js
@@ -9,16 +9,41 @@ import { connect } from 'react-redux';
 import TaskList from './tasklist';
 import Spinner from './spinner'
 //import People from './people';
+import { StackNavigator } from 'react-navigation';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {loadingTasks: true};
+  }
+
+  componentWillMount(){
+    setTimeout(() => {
+        this.setState({loadingTasks: false})
+        console.log('Timeout ended after 3sec');      
+      }, 3000
+    );
+  }
+
+  renderMainApp(){
+    if(!this.state.loadingTasks){
+      return (<TaskList/>);
+    } else {
+      return (<Spinner/>);
+    }
+  }
   render() {
+    debugger;
     return (
       <View>
-          <TaskList/>
-          <Spinner/>
+        {
+          this.renderMainApp()
+        }
       </View>
     )
   }
 }
+
+
 
 export default App;
